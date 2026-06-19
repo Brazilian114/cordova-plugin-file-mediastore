2d4c243913182ce5a61e4e78bdc51170bbed617e
Brazilian114 <32619028+Brazilian114@users.noreply.github.com>
2026-06-19 09:46:37 +0700

    Update FileMediastore.js

--- www/FileMediastore.js
+++ www/FileMediastore.js
@@ -1,9 +1,20 @@
 var exec = require('cordova/exec');
 
+function call(action, options, success, error) {
+  if (typeof success === 'function' || typeof error === 'function') {
+    exec(success, error, 'FileMediastore', action, [options || {}]);
+    return;
+  }
+
+  return new Promise(function (resolve, reject) {
+    exec(resolve, reject, 'FileMediastore', action, [options || {}]);
+  });
+}
+
 exports.writeFile = function (options, success, error) {
-  exec(success, error, 'FileMediastore', 'writeFile', [options || {}]);
+  return call('writeFile', options, success, error);
 };
 
 exports.saveFile = function (options, success, error) {
-  exec(success, error, 'FileMediastore', 'saveFile', [options || {}]);
+  return call('saveFile', options, success, error);
 };
